4fc804978ecd677a18064a2bcfb0084bb2b570be
unknown <1gulu1@protonmail.com>
2020-10-05 11:08:54 +0200

    refactoring

--- bead.js
+++ bead.js
@@ -7,15 +7,21 @@ class Bead {
     }
 
     show() {
+        push();
+        translate(width / 2, height / 2);
         fill(this.x + 50, this.s + 50, this.y + 50);
         stroke(this.x + 50, this.s + 50, this.y + 50);
         strokeWeight(4);
         this.x = lerp(this.x, x, this.s);
         this.y = lerp(this.y, y, this.s);
         ellipse(this.x, this.y, this.r * 2, this.r * 2);
+        pop();
     }
 
     connect(x_, y_) {
+        push();
+        translate(width / 2, height / 2);
         line(this.x, this.y, x_, y_);
+        pop();
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,19 +1,18 @@
-let beads = [];
+const beads = [];
 let x;
 let y;
 let a = 0;
 let capture;
 let recording = false;
 let gif;
-let c;
 
 function setup() {
-    c = createCanvas(innerWidth, innerHeight);
+    createCanvas(innerWidth, innerHeight);
     //capture = createCapture(VIDEO);
     // capture.size(width, height);
     //capture.hide();
-    for (let i = 1; i < 20; i++) {
-        beads[i] = new Bead(0, 0, i * 2, 1 / i);
+    for (let i = 0; i < 20; i++) {
+        beads[i] = new Bead(width / 2, height / 2, i * 4, 1 / i);
     }
 }
 
@@ -21,23 +20,12 @@ function draw() {
     background(0);
     fill(255);
     noStroke();
-    translate(width / 2, height / 2);
-
-
 
     x = sin(a) * 500;
     y = (pow(cos(a), 2) * 200) - 50;
-    scale(1.5);
-    for (let i = 1; i < beads.length; i++) {
-        beads[i].show();
-        //beads[i].connect(mouseX - width / 2, mouseY - height / 2);
+    for (const b of beads) {
+        b.show();
+        b.connect(mouseX, mouseY);
     }
     a += 0.055;
-
 }
-
-function mousePressed() {
-    saveFrames('out', 'png', 5, 60);
-}
-
-console.log(beads);
